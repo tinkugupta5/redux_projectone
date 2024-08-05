78e445d6764f57c195d826be5a515870479ff1e3
tinkugupta5 <tinku.gupta4@gmail.com>
2024-08-05 17:44:23 +0530

    chnages

--- src/reduxContainer/BookReducer.js
+++ src/reduxContainer/BookReducer.js
@@ -3,6 +3,8 @@ const initialState = {
     NumberOfBooks: 20
 }
 
+// pures function chnages are made with pure function 
+
 const BookReducer = (state = initialState, action) => {
     switch (action.type) {
         case buy_book: return {
