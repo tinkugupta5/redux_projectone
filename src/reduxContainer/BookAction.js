8dc697b5a2c4005bca80ad801d33d23bc6458c42
tinkugupta5 <tinku.gupta4@gmail.com>
2024-08-07 23:04:40 +0530

    changes

--- src/reduxContainer/BookAction.js
+++ src/reduxContainer/BookAction.js
@@ -1,9 +1,12 @@
 import { buy_book } from "./BookTypes";
 // purchase_book is action name 
+// Action creator function that returns action objects.
 const purchase_book = () => {
+
     return {
         type : buy_book
     }
+    
 }
 
 export default purchase_book
